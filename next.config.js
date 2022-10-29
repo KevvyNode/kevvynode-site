8613e952252903182585c4afc0482d73fab5e344
Kevin D <kevin.diangelis@gmail.com>
2022-10-28 22:51:18 -0400

    update build system

--- next.config.js
+++ next.config.js
@@ -5,7 +5,15 @@ module.exports =  {
         keepAlive: false,
     },
     generateBuildId: async () => {
-        
+         const octokit = new Octokit({
+             auth: process.env.auth
+        })
+
+        await octokit.request('GET /repos/{owner}/{repo}/commits/{ref}', {
+            owner: 'ItsCryptic',
+            repo: 'kevvynode-site',
+            ref: 'main'
+        })
         return 'Testing'
     }
 }
