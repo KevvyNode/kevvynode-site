edbd8df036b209137e8aaab82ca24504697c65b9
Kevin D <kevin.diangelis@gmail.com>
2022-10-29 13:08:00 -0400

    very basic stuff, also add a node version file!

---
+++ pages/services.js
@@ -0,0 +1,14 @@
+import Header from '../components/header'
+import Footer from '../components/footer'
+
+function services() {
+    return (
+        <>
+        <Header></Header>
+        <Footer></Footer>
+        </>
+    )
+
+}
+
+export default services;
