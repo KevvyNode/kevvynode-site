d44ce3ea4ca786baca193fd99c164d556d2a481d
Kevin D <kevin.diangelis@gmail.com>
2022-10-28 22:47:32 -0400

    Revert "update build system & license information"
    This reverts commit e8ba4a75dc29f85c21d75a312b6489c0e91b2b32.

--- next.config.js
+++ next.config.js
@@ -1,20 +1,11 @@
 const { default: build } = require("next/dist/build");
-const { Octokit } = require("octokit");
 
 module.exports =  {
     httpAgentOptions: {
         keepAlive: false,
     },
     generateBuildId: async () => {
-        const octokit = new Octokit({
-            auth: process.env.auth
-        })
-
-        await octokit.request('GET /repos/{owner}/{repo}/commits/{ref}', {
-            owner: 'ItsCryptic',
-            repo: 'kevvynode-site',
-            ref: 'main'
-        })
+        
         return 'Testing'
     }
 }
