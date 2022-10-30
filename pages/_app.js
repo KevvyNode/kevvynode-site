915c5962c63be7c0220110cc8883466dc8e316bd
Kevin D <kevin.diangelis@gmail.com>
2022-10-29 21:48:27 -0400

    lots of misc changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../css/css.css';
+import Image from 'next/image';
 
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
--- pages/services.js
+++ pages/services.js
@@ -1,10 +1,16 @@
 import Header from '../components/header'
 import Footer from '../components/footer'
+import '../css/services.module.css';
+
+
 
 function services() {
     return (
         <>
         <Header></Header>
+        <div id="services-block">
+            
+        </div>
         <Footer></Footer>
         </>
     )
