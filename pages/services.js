65d6d89489d61c862dbd9eec18aba148d1e2453f
Kevin D <kevin.diangelis@gmail.com>
2022-11-01 13:17:51 +0000

    just an update commit

--- pages/services.js
+++ pages/services.js
@@ -1,5 +1,6 @@
 import Header from '../components/header'
 import Footer from '../components/footer'
+import Head from 'next/head';
 import '../css/services.module.css';
 
 
@@ -7,6 +8,10 @@ import '../css/services.module.css';
 function services() {
     return (
         <>
+    <Head>
+        <title>KevvyNode - Services</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+    </Head>
         <Header></Header>
         <div id="services-block">
             <p>test</p>
