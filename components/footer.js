152e568a1eb495ec5cf7ed7eed9a2ee82e2fa84e
Kevin D <kevin.diangelis@gmail.com>
2022-10-31 23:32:58 -0400

    fix footer!

--- components/footer.js
+++ components/footer.js
@@ -1,14 +1,15 @@
 import '../css/footer.module.css';
-import Script from 'next/script';
 
 const Footer = () => {
     return (
-        <div id="footer">
+        <>
+            <div id="footer">
             <p id="footer-emails">Abuse: abuse@kevvynode.org<br />Sales: sales@kevvynode.org</p>
             <p>Current build: <span id="current-commit"></span></p>
             # https://docs.github.com/en/rest/commits/commits#get-a-commit
-            <Script type="module" src="../js/footer.module.js></Script>
         </div>
+        
+        </>
     );
 }
 
